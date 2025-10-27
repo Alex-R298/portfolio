63d487e90facc63d3cad66623d1cb4639de8ac9a
Alex-R298 <alexreitz298@gmail.com>
2025-10-27 23:03:35 +0100

    Add skill icons for various technologies
    - Added Firebase icon
    - Added Git icon
    - Added HTML icon
    - Added JavaScript (Js) icon
    - Added Material Design icon
    - Added Scrum icon
    - Added TypeScript (Ts) icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./shared/footer/footer.component";
+import { NavbarComponent } from "./shared/navbar/navbar.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,
+    FooterComponent, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'portfolio';
--- src/app/portfolio-page/portfolio-page.component.ts
+++ src/app/portfolio-page/portfolio-page.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from './hero/hero.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillSetComponent } from './skill-set/skill-set.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
+import { ColleaguesComponent } from './colleagues/colleagues.component';
 
 @Component({
   selector: 'app-portfolio-page',
   standalone: true,
-  imports: [],
+  imports: [AboutMeComponent, HeroComponent ,SkillSetComponent, ProjectsComponent, ContactMeComponent, ColleaguesComponent ],
   templateUrl: './portfolio-page.component.html',
   styleUrl: './portfolio-page.component.scss'
 })
 export class PortfolioPageComponent {
-
 }
