62fd994c6a2b4a441b4d89bc3e55fc031c7b3232
Alex-R298 <alexreitz298@gmail.com>
2025-10-29 02:11:03 +0100

    Refactor code structure for improved readability and maintainability

--- src/app/portfolio-page/skill-set/skill-set.component.ts
+++ src/app/portfolio-page/skill-set/skill-set.component.ts
@@ -8,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrl: './skill-set.component.scss'
 })
 export class SkillSetComponent {
+  skills: { img: string, title: string }[] = [
+    { img : "CSS.png",
+      title: "CSS",
+  },
+    { img : "Js.png",
+      title: "JavaScript",
+  },
+    { img : "Ts.png",
+      title: "TypeScript",
+  },
+    { img : "Angular.png",
+      title: "Angular",
+  },
+    { img : "Scrum.png",
+      title: "Scrum",
+  },
+    { img : "Material-Design.png",
+      title: "Material Design",
+  },
+  { img : "Git.png",
+      title: "Git",
+  },
+  { img : "Firebase.png",
+      title: "Firebase",
+  },
+  { img : "HTML.png",
+      title: "HTML",
+  }
+];
 
 }
