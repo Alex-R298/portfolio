f0abc3ef9e27ac111a38912b48e4e6e50f424f2e
Alex-R298 <alexreitz298@gmail.com>
2025-11-06 20:49:53 +0100

    Refactor code structure for improved readability and maintainability

--- src/app/portfolio-page/projects/projects.component.ts
+++ src/app/portfolio-page/projects/projects.component.ts
@@ -12,13 +12,49 @@ import { CommonModule } from '@angular/common';
 })
 export class ProjectsComponent {
   showProjectCard = false;
+  selectedProject: any = null;
+  currentProjectIndex = 0;
 
   constructor(private renderer: Renderer2) {}
 
-  projectDetails() {
+  projects = [
+    {
+      name:'Join',
+      description:'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ',
+      implementation:'Short text that describes your role or the workflow for this specific project. Let a recruiter know more about your knowledge and ability to work independently or collaboratively in a structured way.',
+      duration:'5 weeks',
+      imgSkills:['CSS.png','HTML.png','Js.png','Firebase.png'],
+      img:'Component 301.png'
+    },
+    {
+      name:'El Pollo Loco',
+      description:'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen. ',
+      implementation:'Short text that describes your role or the workflow for this specific project. Let a recruiter know more about your knowledge and ability to work independently or collaboratively in a structured way.',
+      duration:'3 weeks',
+      imgSkills:['CSS.png','HTML.png','Js.png','Firebase.png'],
+      img:'28_Project thumbnails.png'
+    },
+    {
+      name:'DABubble',
+      description:'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ',
+      implementation:'This App is a Slack Clone App. It revolutionizes team communication and collaboration with its intuitive interface, real-time messaging, and robust channel organization.',
+      duration:'4 weeks',
+      imgSkills:['CSS.png','HTML.png','Ts.png','Angular.png'],
+      img:'Component 30.png'
+    },
+  ]
+
+  projectDetails(project: any) {
     this.showProjectCard = true;
+    this.selectedProject = project;
+    this.currentProjectIndex = this.projects.indexOf(project);
     // Scrollen verhindern
     this.renderer.setStyle(document.body, 'overflow', 'hidden');
+  }
+
+  nextProject() {
+    this.currentProjectIndex = (this.currentProjectIndex + 1) % this.projects.length;
+    this.selectedProject = this.projects[this.currentProjectIndex];
   }
 
   closeProjectCard() {
--- src/app/shared/project-card/project-card.component.ts
+++ src/app/shared/project-card/project-card.component.ts
@@ -1,17 +1,26 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-project-card',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, CommonModule],
   templateUrl: './project-card.component.html',
   styleUrl: './project-card.component.scss'
 })
 export class ProjectCardComponent {
+
+
+  @Input() projectData: any;
   @Output() close = new EventEmitter<void>(); // ← Event zum Parent senden
+  @Output() next = new EventEmitter<void>();
 
   closeCard() {
     this.close.emit(); // ← Sagt dem Parent: "Schließ mich!"
+  }
+
+  nextProject() {
+    this.next.emit();
   }
 }
