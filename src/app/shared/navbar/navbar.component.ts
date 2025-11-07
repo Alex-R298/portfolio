86e5dbb249163f282c5c7b9d223150888cf2ea89
Alex-R298 <alexreitz298@gmail.com>
2025-11-08 00:57:25 +0100

    Refactor navbar and project card components for improved functionality and styling; update image assets and enhance layout consistency across the portfolio page.

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  @Output() navClose = new EventEmitter<void>();
 
+ isGerman: boolean = true; // false = EN, true = DE
+
+  closeNav() {
+    this.navClose.emit();
+  }
+
+  toggleLanguage() {
+    this.isGerman = !this.isGerman;
+  }
 }
