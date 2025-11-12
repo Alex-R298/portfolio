80996d16a4d8521ae62535e75b1311d1eaef5a7e
Alex-R298 <alexreitz298@gmail.com>
2025-11-12 20:59:47 +0100

    Refactor styles and improve responsiveness across various components
    - Updated SCSS for skill-set component to enhance layout and responsiveness for different screen sizes.
    - Modified footer component HTML structure to group social links and adjusted styles for better alignment.
    - Improved footer styles for better responsiveness and hover effects.
    - Translated navbar links to German and adjusted styles for mobile responsiveness.
    - Enhanced project card styles for better layout and responsiveness on mobile and tablet devices.

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-legal-notice',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
-export class LegalNoticeComponent {
+export class LegalNoticeComponent implements OnInit {
+  constructor(
+    private location: Location,
+    private router: Router
+  ) {}
 
+  ngOnInit() {
+    window.scrollTo(0, 0);
+  }
+
+  goBack() {
+    this.router.navigate(['/'], { fragment: 'top' }).then(() => {
+      window.scrollTo(0, 0);
+    });
+  }
 }
--- src/app/portfolio-page/projects/projects.component.ts
+++ src/app/portfolio-page/projects/projects.component.ts
@@ -15,38 +15,37 @@ export class ProjectsComponent {
   selectedProject: any = null;
   currentProjectIndex = 0;
 
-  constructor(private renderer: Renderer2) {}
+  constructor(private renderer: Renderer2) { }
 
   projects = [
     {
-      name:'Join',
-      description:'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ',
-      implementation:'Short text that describes your role or the workflow for this specific project. Let a recruiter know more about your knowledge and ability to work independently or collaboratively in a structured way.',
-      duration:'5 weeks',
-      imgSkills:['CSS.png','HTML.png','Js.png','Firebase.png'],
-      skillsNames:['CSS','HTML','JavaScript','Firebase'],
-      img:'join.png',
+      name: 'Join',
+      description: 'Task-Manager inspiriert vom Kanban-System. Erstelle und organisiere Aufgaben mit Drag-and-Drop-Funktionen, weise Benutzer und Kategorien zu.',
+      implementation: 'Kurzer Text, der deine Rolle oder den Workflow für dieses spezifische Projekt beschreibt. Lass einen Recruiter mehr über dein Wissen und deine Fähigkeit erfahren, eigenständig oder kollaborativ in einer strukturierten Weise zu arbeiten.',
+      duration: '5 Wochen',
+      imgSkills: ['CSS.png', 'HTML.png', 'Js.png', 'Firebase.png'],
+      skillsNames: ['CSS', 'HTML', 'JavaScript', 'Firebase'],
+      img: 'join.png',
       lineWidth: 120,
       lineHeight: 20
     },
     {
-      name:'Pixel Quest',
-      description:'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen. ',
-      implementation:'Short text that describes your role or the workflow for this specific project. Let a recruiter know more about your knowledge and ability to work independently or collaboratively in a structured way.',
-      duration:'3 weeks',
-      imgSkills:['CSS.png','HTML.png','Js.png'],
-      skillsNames:['CSS','HTML','JavaScript'],
-      img:'pixel-game.png',
+      name: 'Pixel Quest',
+      description: 'Spring-, Lauf- und Wurfspiel basierend auf objektorientiertem Ansatz. Hilf Pepe, Münzen und Tabasco-Salsa zu finden, um gegen das verrückte Huhn zu kämpfen.',
+      implementation: 'Kurzer Text, der deine Rolle oder den Workflow für dieses spezifische Projekt beschreibt. Lass einen Recruiter mehr über dein Wissen und deine Fähigkeit erfahren, eigenständig oder kollaborativ in einer strukturierten Weise zu arbeiten.',
+      duration: '3 Wochen',
+      imgSkills: ['CSS.png', 'HTML.png', 'Js.png'],
+      skillsNames: ['CSS', 'HTML', 'JavaScript'],
+      img: 'pixel-game.png',
       lineWidth: 340,
       lineHeight: 20
     },
     // {
-    //   name:'DABubble',
-    //   description:'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ',
-    //   implementation:'This App is a Slack Clone App. It revolutionizes team communication and collaboration with its intuitive interface, real-time messaging, and robust channel organization.',
-    //   duration:'4 weeks',
-    //   imgSkills:['CSS.png','HTML.png','Ts.png','Angular.png'],
-    //   img:'Component 30.png'
+    // name: 'DABubble',
+    // description: 'Diese App ist ein Slack-Clone. Sie revolutioniert Team-Kommunikation und Zusammenarbeit mit ihrer intuitiven Oberfläche, Echtzeit-Messaging und robuster Channel-Organisation.',
+    // implementation: 'Kurzer Text, der deine Rolle oder den Workflow für dieses spezifische Projekt beschreibt. Lass einen Recruiter mehr über dein Wissen und deine Fähigkeit erfahren, eigenständig oder kollaborativ in einer strukturierten Weise zu arbeiten.'
+    // imgSkills: ['CSS.png','HTML.png','Ts.png','Angular.png'],
+    // img: 'Component 30.png'
     // },
   ]
 
