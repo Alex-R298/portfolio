b9bdf32fe827db7edbd22ff7be6e2b789de21b94
Alex-R298 <alexreitz298@gmail.com>
2025-11-22 22:50:51 +0100

    Update email addresses to new domain for consistency across components

--- src/app/sendMail.php
+++ src/app/sendMail.php
@@ -18,7 +18,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $name = $params->name;
             $message = $params->message;
     
-            $recipient = 'alexreitz298@gmail.com';  
+            $recipient = 'info@alex-reitz.de';
             $subject = "Contact From <$email>";
             $message = "From:" . $name . "<br>" . $message ;
     
